e0ae1a4343ce199c87c11a9ec937ae4eacf51081
Drew Morrow <drew4493@icloud.com>
2025-11-04 09:30:33 -0500

    Implement TestGP class for genetic algorithm simulation
    This class runs a genetic algorithm simulation, prompting for a data file and iterating through generations.

---
+++ TestGP.java
@@ -0,0 +1,18 @@
+public class TestGP {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter data file name: ");
+        String fileName = sc.nextLine();
+
+        Generation gen = new Generation(500, 3, fileName);
+        gen.evalAll();
+
+        for (int i = 1; i <= 50; i++) {
+            System.out.println("\nGeneration " + i + ":");
+            gen.printBestTree();
+            gen.printBestFitness();
+            gen.evolve();
+            gen.evalAll();
+        }
+    }
+}
