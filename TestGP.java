e4117385a5580d58cb936ef6aa35dbe902de2e15
Drew Morrow <drew4493@icloud.com>
2025-12-04 09:57:16 -0500

    Update TestGP.java
    Refactor TestGP to accept command line arguments for file name and adjust generation parameters.

--- TestGP.java
+++ TestGP.java
@@ -4,20 +4,48 @@ import binary.GPTree;
 import java.util.ArrayList;
 
 public class TestGP {
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter data file name: ");
-        String fileName = sc.nextLine();
 
-        Generation gen = new Generation(500, 3, fileName);
-        gen.evalAll();
+        String fileName;
 
-        for (int i = 1; i <= 50; i++) {
-            System.out.println("\nGeneration " + i + ":");
+        if (args.length >= 1) {
+            fileName = args[0];
+        } 
+        else {
+            Scanner keyboard = new Scanner(System.in);
+            System.out.println("Enter the data file name:");
+            fileName = keyboard.nextLine();
+        }
+
+        int generationSize = 500;
+        int maxDepth = 6;
+
+        Generation gen = new Generation(generationSize, maxDepth, fileName);
+
+        int totalGenerations = 50;
+
+        for (int g = 1; g <= totalGenerations; g++) {
+
+            gen.evalAll();
+
+            System.out.println("Generation " + g + ":");
             gen.printBestTree();
             gen.printBestFitness();
+            ArrayList<GPTree> topTen = gen.getTopTen();
+            System.out.print("Top 10 fitness values: ");
+            for (int i = 0; i < topTen.size(); i++) {
+                double fit = topTen.get(i).getFitness();
+                System.out.printf("%.2f", fit);
+                if (i < topTen.size() - 1) {
+                    System.out.print(", ");
+                }
+            }
+            System.out.println();
+
+            System.out.println("------");
+
             gen.evolve();
-            gen.evalAll();
         }
     }
 }
